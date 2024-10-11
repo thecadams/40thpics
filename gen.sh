63500bf18f8832d50414cd1d73295240eff51e8e
Chris Adams <chris@cadams.com.au>
2024-10-11 18:05:37 +1100

    Initial commit without media

---
+++ gen.sh
@@ -0,0 +1,8 @@
+thumbsup \
+ --input "$INPUT" \
+ --output "$(dirname $0)" \
+ --title "Chris and Emma's 40th Photos" \
+ --theme cards \
+ --photo-preview copy \
+ --photo-download copy \
+ --footer '<p>Original galleries:</p><ul style="list-style: inside"><li><a href="https://photos.google.com/share/AF1QipNHwzZQgsGFTpjtFWq0ggyjqJrsxY68P3SEX-nIB-DIA2BMkxmfn-IbbOq6pbgH3Q?key=dnIwRHpjZzJJUHNpVFhzU1JWTi1YWjVLNFRSWFBn" target="_blank">Josh Sharman'"'"'s Pics - Google Photos</a></li><li><a href="http://dropbox.com/scl/fo/dxzrc2nmwqjfzznr874xi/AJMQebDPN287KUX1L99MlPo?rlkey=aic3w1pcn9h7r4cglzayvtu7a&e=1&st=xn9xtugo&dl=0" target="_blank">Adam Leo'"'"'s Photos - Dropbox</a></li></ul><p style="padding-top: 1rem">Photographers'"'"' Instagram pages - make sure to follow &amp; slide into their DMs!</p><ul style="list-style: inside"><li><a href="https://instagram.com/josh.sharman" target="_blank">Josh Sharman</a></li><li><a href="https://www.instagram.com/tkleophoto" target="_blank">Adam Leo</a></li></ul>'
